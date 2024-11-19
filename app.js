3c0a5f7593da14384bc8d7aa1d4998d732e6c779
itsathefak <athefayubkhan@gmail.com>
2024-11-19 12:36:35 -0500

    Create a Delete route to delete review

--- app.js
+++ app.js
@@ -149,6 +149,18 @@ app.post(
   })
 );
 
+// Delete Route
+app.delete(
+  "/listings/:id/reviews/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+  })
+);
+
 // Page not found error
 
 app.all("*", (req, res, next) => {
