546af2f47a33b5f47e547cb64fd29faaef5a85ab
itsathefak <athefayubkhan@gmail.com>
2024-11-20 14:41:28 -0500

    Add cookie and cookie option

--- app.js
+++ app.js
@@ -35,6 +35,12 @@ const sessionOptions = {
   secret: "secretcode",
   resave: false,
   saveUninitialized: true,
+  cookie: {
+    //Cookie Options
+    expires: Date.now() * 7 * 24 * 60 * 60 * 1000, //Always in milliseconds
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true, //cross Scripting attack Protection
+  },
 };
 
 app.use(session(sessionOptions));
