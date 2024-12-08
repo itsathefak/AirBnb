760df0d59985464e0b09df519913de428e428da2
itsathefak <athefayubkhan@gmail.com>
2024-12-08 11:03:11 -0500

    Manipulating form using multer for image files

--- routes/listing.js
+++ routes/listing.js
@@ -2,18 +2,20 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync");
 const { isLoggedIn, isOwner, validateListing } = require("../middelware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 // Index route
-router
-  .route("/")
-  .get(wrapAsync(listingController.index))
-  .post(
-    isLoggedIn,
-    validateListing,
-    wrapAsync(listingController.createListing)
-  );
+router.route("/").post(upload.single("listing[image]"), (req, res) => {
+  res.send(req.file);
+});
+// .get(wrapAsync(listingController.index))
+// .post(
+//   isLoggedIn,
+//   validateListing,
+//   wrapAsync(listingController.createListing)
+// );
 
 // New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
