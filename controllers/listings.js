9fa1960a4e70d2919557abce433d834ce412a473
itsathefak <athefayubkhan@gmail.com>
2024-12-14 12:29:12 -0500

    Storing Coordinates for mapbox

--- controllers/listings.js
+++ controllers/listings.js
@@ -37,6 +37,7 @@ module.exports.createListing = async (req, res, next) => {
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
   newListing.image = { url, filename };
+  newListing.geometry = res.body.features[0].geometry;
   await newListing.save();
   req.flash("success", "New Listing Created");
   res.redirect("/listings");
