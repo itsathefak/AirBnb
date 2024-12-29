6b0543723b4480085b796de326d06da48a648d16
itsathefak <athefayubkhan@gmail.com>
2024-12-28 21:58:18 -0500

    Set default image for listing

--- controllers/listings.js
+++ controllers/listings.js
@@ -79,7 +79,9 @@ module.exports.createListing = async (req, res, next) => {
     const geometry = geoResponse.body.features[0].geometry;
 
     // If the image URL is provided, use it
-    const imageUrl = req.body.listing.imageUrl || "";
+    const imageUrl =
+      req.body.listing.imageUrl ||
+      "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
     const newListing = new Listing({
       ...req.body.listing,
